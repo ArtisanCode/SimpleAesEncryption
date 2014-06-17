029333e17e66ff14b357095504776c6483f528fe
Tom-Kuhn <tom.c.kuhn@gmail.com>
2014-06-17 13:44:45 +0100

    Add KeyGen tool to the solution

---
+++ src/KeyGen/Program.cs
@@ -0,0 +1,89 @@
+﻿using ArtisanCode.SimpleAesEncryption;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace ArtisanCode.SimpleAES.KeyGen.KeyGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int keySize = 256;
+
+            // If the user specified a non default key length
+            if(args.Any())
+            {
+                // If there are missing or additional command-line parameters
+                if(args.Length != 2)
+                {
+                    ExitWithError(1);
+                }
+
+                // If the length switch was not selected
+                if(!args[0].Equals("-L", StringComparison.OrdinalIgnoreCase))
+                {
+                    ExitWithError(1);
+                }
+
+                // Parse the keySize from the command-line parameters
+                // If there was an error, exit
+                if (string.IsNullOrWhiteSpace(args[1]) || !Int32.TryParse(args[1], out keySize))
+                {
+                    ExitWithError(1);
+                }
+            }
+
+            Console.WriteLine("Generating a new key for Log4Net.MessageEncryptor: ");
+
+            // Read the configuration file for the key size information
+            SimpleAesEncryptionConfiguration config = new SimpleAesEncryptionConfiguration() {
+                EncryptionKey = new EncryptionKeyConfigurationElement(keySize, "")
+            };
+
+            using (RijndaelManaged cryptoContainer = new RijndaelManaged())
+            {
+                var legalKeys = new [] {128,192,256};
+                // Validate KeySize
+                if(!legalKeys.Contains(keySize))
+                {
+                    Console.WriteLine("Invalid Key size (" + keySize + ")");
+                    Console.WriteLine("Valid Key sizes are: " + string.Join(", ", legalKeys));
+
+                    return;
+                }
+
+                cryptoContainer.KeySize = config.EncryptionKey.KeySize;
+
+                // Generates a new key using the standard .NET method of generating a new symmetric key
+                cryptoContainer.GenerateKey();
+
+                var key = Convert.ToBase64String(cryptoContainer.Key);
+
+                // Output the new key to the screen and the clipboard
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine();
+                Console.WriteLine(key);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Please press any key to exit.");
+            Console.ReadKey();
+        }
+
+        public static void ExitWithError(int errorCode)
+        {
+            WriteUsageStatement();
+            Environment.Exit(errorCode);
+        }
+
+        public static void WriteUsageStatement()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("No Arguments : Use the default key length of 256 bits (recommended)");
+            Console.WriteLine("-l or -L : Specifies the key length in 128, 182, or 256)");
+        }
+    }
+}
--- src/KeyGen/Properties/AssemblyInfo.cs
+++ src/KeyGen/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleAesEncryption")]
-[assembly: AssemblyDescription("A library to allow you to use AES encryption with minimum effort")]
+[assembly: AssemblyTitle("KeyGen")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ArtisanCode")]
-[assembly: AssemblyProduct("SimpleAesEncryption")]
-[assembly: AssemblyCopyright("Copyright © ArtisanCode 2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KeyGen")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ad766d4b-d193-4fd6-9acb-3f328edd7001")]
+[assembly: Guid("669b7a6e-2bf7-4d44-aef5-5076befb19f0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.*")]
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
